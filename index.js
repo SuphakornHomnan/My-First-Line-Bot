8dabb326fa18de7ffa8ad47c5db9472c49118b70
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-01-26 21:23:40 +0700

    [Config] line messaging

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ const app = express();
 const api = require("./routes/index");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const config = {
+  channelAccessToken: `${process.env.CHANNEL_ACCESS_TOKEN}`,
+  channelSecret: `${process.env.CHANNEL_SECRET}`,
+};
+require("dotenv").config();
 app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", api);
+
+console.log(process.env.CHANNEL_SECRET);
+console.log(process.env.CHANNEL_ACCESS_TOKEN);
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
+}
+
 app.use("/", (req, res) => {
   res.send("Hello everyone");
 });
