09d18235c97e4247651d8ff2c0088a1133f04136
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-01-26 20:52:13 +0700

    [Added] root path

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
+const api = require("./routes/index");
 
+app.use("/api", api);
 app.listen(8080, () => {
   console.log(`server run on port 8080`);
 });
