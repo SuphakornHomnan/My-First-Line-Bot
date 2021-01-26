8ea69b1b06e5485417ba13dfe0a422bfd4bb4d51
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-01-26 21:50:54 +0700

    [Changed]

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const api = require("./routes/index");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const config = {
-  channelAccessToken: `${process.env.CHANNEL_ACCESS_TOKEN}`,
-  channelSecret: `${process.env.CHANNEL_SECRET}`,
+  channelAccessToken: "6d5885772a614b984fd421e8193030ef",
+  channelSecret:
+    "exBaua04Is156G9zTpYwWOHZ6G7fSErmWVb52ft27laxkvlteQ16YoreJXMS8X0onZ7wQ+/saIZf6nhffBeX8bhXt42hpVF9kMKpQK7oWg0GSvN9+jBbxixWzciyWRr/yuuu9wY1s6NhoqJjAYK4uAdB04t89/1O/w1cDnyilFU=",
 };
-require("dotenv").config();
+
 app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", api);
 
-console.log(process.env.CHANNEL_SECRET);
-console.log(process.env.CHANNEL_ACCESS_TOKEN);
 app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
