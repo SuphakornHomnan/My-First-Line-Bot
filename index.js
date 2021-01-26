3c7f0f327d2b71a2c2cef7c42c4c447be40aba02
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-01-26 20:57:33 +0700

    [Added] setting json object

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
 const api = require("./routes/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+app.use(cors({ credentials: true, origin: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api", api);
+app.use("/", (req, res) => {
+  res.send("Hello everyone");
+});
 app.listen(8080, () => {
   console.log(`server run on port 8080`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,5 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.send("Hello Line Bot");
 });
+
+module.exports = router;
