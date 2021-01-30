427e913ad7848a66a89b2bde0f0168c4af2a56af
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-01-31 00:15:04 +0700

    [Fixed] add async function

--- helpers/send_response.js
+++ helpers/send_response.js
@@ -1,36 +1,34 @@
 const healths = require("../health");
 const guardians = require("../guardian");
-const payments = require("../payment")
-exports.send_payment_info = (line_id) => {
+const payments = require("../payment");
+exports.send_payment_info = async (line_id) => {
   try {
-      const find_child_from_guardian = await guardians.findOne({
-        line_id,
-      });
-      const info = await payments.find({child:find_child_from_guardian.child})
-      console.log(info);
-      if (info.length>1) {
-          var temp = []
-          for (let i = 0; i < info.length; i++) {
-              if (info[i].outstanding_balance===0) {
-                  temp.push(
-                      `รายการ${info[i].topic} ชำระครบแล้ว`
-                  )
-              } else {
-                temp.push(
-                  `รายการ${info[i].topic} , ค้างชำระ ${info[i].outstanding_balance}บาท` 
-               ) 
-              }
-          }
-          return temp
-      } else if(info.length===0) {
-          return null
-      }else{
-        if (info[0].outstanding_balance===0) {
-            return `รายการ${info[0].topic} ชำระครบแล้ว`
-        }else{
-            return `รายการ${info[0].topic} , ค้างชำระ ${info[0].outstanding_balance}บาท`
+    const find_child_from_guardian = await guardians.findOne({
+      line_id,
+    });
+    const info = await payments.find({ child: find_child_from_guardian.child });
+    console.log(info);
+    if (info.length > 1) {
+      var temp = [];
+      for (let i = 0; i < info.length; i++) {
+        if (info[i].outstanding_balance === 0) {
+          temp.push(`รายการ${info[i].topic} ชำระครบแล้ว`);
+        } else {
+          temp.push(
+            `รายการ${info[i].topic} , ค้างชำระ ${info[i].outstanding_balance}บาท`
+          );
         }
       }
+      return temp;
+    } else if (info.length === 0) {
+      return null;
+    } else {
+      if (info[0].outstanding_balance === 0) {
+        return `รายการ${info[0].topic} ชำระครบแล้ว`;
+      } else {
+        return `รายการ${info[0].topic} , ค้างชำระ ${info[0].outstanding_balance}บาท`;
+      }
+    }
   } catch (error) {
     console.log("[ERROR] at send_health_info_func");
     console.log(error);
@@ -43,7 +41,7 @@ exports.send_health_info = async (line_id) => {
     const find_child_from_guardian = await guardians.findOne({
       line_id,
     });
-    
+
     console.log(moment().format("YYYY-MM-DD"));
     const info = await healths.findOne({
       date: moment().format("YYYY-MM-DD") + "T00:00:00.000+00:00",
